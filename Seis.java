bee7776dcbdec1d171cfa8dfd35c5280c6852984
Beatriz Helena de moura lima <168425488+BeatrizHelenakima@users.noreply.github.com>
2024-05-22 10:35:53 -0300

    Create Seis.java

---
+++ Seis.java
@@ -0,0 +1,22 @@
+package pacote;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner ref = new Scanner(System.in);
+		
+		 System.out.println(" Insira um número: "); 
+		    int num = ref.nextInt();
+
+		    if (num % 2 == 0) {
+		      System.out.println( "O número " + num + "é potencia de 2");
+		    } else { 
+		      System.out.println("O número " + num + "não é uma potencia de 2");
+		     }
+		    }
+		  }
+
+
+		
