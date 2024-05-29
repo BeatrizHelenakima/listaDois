279144ce026e77df3ffd63eeddb017e8e19b1cd9
Beatriz Helena de moura lima <168425488+BeatrizHelenakima@users.noreply.github.com>
2024-05-29 10:38:49 -0300

    Create trintaENove.java

---
+++ trintaENove.java
@@ -0,0 +1,29 @@
+package pacote;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner ref = new Scanner(System.in);
+				
+		int area = 0;
+		
+    	   System.out.println(" insira sua nota:"); 
+		    int notau = ref.nextInt();
+		   System.out.println(" insira sua nota:"); 
+		    int notad = ref.nextInt();
+		    System.out.println(" insira sua nota:"); 
+		    int notat = ref.nextInt();
+		    
+		    System.out.println(" Suas notas foram: \n" + notau + "\n" + notad + "\n" + notat); 
+	
+		    
+		    
+		    }
+		 
+			}
+		    
+		    
+		  
+		
