711c269d987298c2d29bb07ddea44f03ad1f6273
Beatriz Helena de moura lima <168425488+BeatrizHelenakima@users.noreply.github.com>
2024-05-29 10:42:24 -0300

    Create cinquenta.java

---
+++ cinquenta.java
@@ -0,0 +1,29 @@
+package pacote;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner ref = new Scanner(System.in);
+				
+		int area = 0;
+		
+    	   System.out.println("ensira a base do terreno:"); 
+		    int base = ref.nextInt();
+		   System.out.println(" ensira a altura do terreno:"); 
+		    int altura = ref.nextInt();
+		   
+		    area= base*altura;
+		    
+		    System.out.println(" a area do terreno é " + area + "cm"); 
+	
+		    
+		    
+		    }
+		 
+			}
+		    
+		    
+		  
+		
